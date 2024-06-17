58b2c82a16a000c68c79f3fe91b22d790e2b5f8f
jeongjinjoo <127469489+jeongjinjoo@users.noreply.github.com>
2024-06-17 18:29:48 +0900

    [Bronze V] Title: 오늘 날짜, Time: 192 ms, Memory: 17144 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,12 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class Main {
+	public static void main(String[] args) {
+		LocalDate date = null;
+		LocalDate now = date.now();
+		System.out.println(now);
+		
+	}
+
+}
