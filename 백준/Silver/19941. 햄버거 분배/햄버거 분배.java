8ec750af70dc067ddc7d521fe9233e265768834b
jeongjinjoo <127469489+jeongjinjoo@users.noreply.github.com>
2024-08-19 02:26:24 +0900

    [Silver III] Title: 햄버거 분배, Time: 68 ms, Memory: 11976 KB -BaekjoonHub

--- 백준/Silver/19941. 햄버거 분배/햄버거 분배.java
+++ 백준/Silver/19941. 햄버거 분배/햄버거 분배.java
@@ -23,23 +23,27 @@ public class Main {
 //        햄버거를 발견했는지의 여부를 저장하는 배열
         char[] result = br.readLine().toCharArray();
 
+//      사람 수 카운트
         int cnt = 0;
 
+//        가장 왼쪽에 있는 것부터 먹어야 다음 사람이 먹을 확률이 높아짐.
+//        i는 식탁의 길이만큼 반복
         for (int i = 0; i < N; i++) {
-            if (result[i] =='P') {
+            if (result[i] =='P') { // 만약 P라면
                 // 가장 먼저 발견한 햄버거 탐색
                 for(int j = i - K; j <= i + K; j++) {
                     if(j < 0 || j >= N){
                         continue;
                     }
-                   if (result[j] == 'H') {
-                       cnt++;
-                       result[j] = 'X';
+                   if (result[j] == 'H') { // 만약 햄버거를 찾는다면
+                       cnt++; // 카운트 수 증가
+                       result[j] = 'X'; // 그 위치 X로 변경해줌. 햄버거가 이미 없어져서 끝.
                        break;
                    }
                 }
             }
         }
+//        카운트한 수 출력
         System.out.println(cnt);
     }
 }
