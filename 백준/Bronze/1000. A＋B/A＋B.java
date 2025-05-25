4e57746e5ddc946e19fdcb18cd628bc89901b9c4
jeongjinjoo <127469489+jeongjinjoo@users.noreply.github.com>
2025-05-25 23:13:03 +0900

    [Bronze V] Title: A+B, Time: 68 ms, Memory: 11528 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int A;
+    static int B;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        A = Integer.parseInt(st.nextToken());
+        B = Integer.parseInt(st.nextToken());
+
+        
+        System.out.println(A + B);
+    }
+
+}
